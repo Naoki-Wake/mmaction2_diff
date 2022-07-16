03e280f32391da8ce5165bc7aa8c0aef7e98bb19
Naoki Wake <naoki.wake@microsoft.com>
2022-07-16 06:38:38 -0700

    slim

--- mmaction_diff/models/__init__.py
+++
@@ -1,4 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-__all__ = [
-    'HEADS'
-]
--- mmaction_diff/models/heads/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) OpenMMLab. All rights reserved.
-from .household_head_nonaddlayer import HOUSEHOLDHead_NONADDLAYER
-from .household_head_addlayer import HOUSEHOLDHead_ADDLAYER
-
-__all__ = [
-    'HOUSEHOLDHead_ADDLAYER', 'HOUSEHOLDHead_NONADDLAYER'
-]
