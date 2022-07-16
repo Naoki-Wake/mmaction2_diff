786b365d33ec582873ceb40990695d653feb79f9
Naoki Wake <naoki.wake@microsoft.com>
2022-07-16 06:17:57 -0700

    slim

--- mmaction_diff/models/__init__.py
+++ mmaction_diff/models/__init__.py
@@ -1,8 +1,4 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .heads import (ACRNHead, AudioTSNHead, AVARoIHead, BaseHead, BBoxHeadAVA,
-                    FBOHead, I3DHead, LFBInferHead, SlowFastHead, STGCNHead,
-                    TimeSformerHead, TPNHead, TRNHead, TSMHead, TSNHead,
-                    X3DHead)
 __all__ = [
     'HEADS'
 ]
